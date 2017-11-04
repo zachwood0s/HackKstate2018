9586dc9d48b0b9458fd920480e5946eff1fc47f9
zachwood0s <zach@hayzak.com>
2017-11-04 12:02:33 -0500

    Created buttons folder

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+var frame_time = 60/1000; // run the local game at 16ms/ 60hz
+if('undefined' != typeof(global)) frame_time = 45; //on server we run at 45ms, 22hz
+
+( function () {
+
+    var lastTime = 0;
+    var vendors = [ 'ms', 'moz', 'webkit', 'o' ];
+
+    for ( var x = 0; x < vendors.length && !window.requestAnimationFrame; ++ x ) {
+        window.requestAnimationFrame = window[ vendors[ x ] + 'RequestAnimationFrame' ];
+        window.cancelAnimationFrame = window[ vendors[ x ] + 'CancelAnimationFrame' ] || window[ vendors[ x ] + 'CancelRequestAnimationFrame' ];
+    }
+
+    if ( !window.requestAnimationFrame ) {
+        window.requestAnimationFrame = function ( callback, element ) {
+            var currTime = Date.now(), timeToCall = Math.max( 0, frame_time - ( currTime - lastTime ) );
+            var id = window.setTimeout( function() { callback( currTime + timeToCall ); }, timeToCall );
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    }
+
+    if ( !window.cancelAnimationFrame ) {
+        window.cancelAnimationFrame = function ( id ) { clearTimeout( id ); };
+    }
+
+}() );
 
 
 var app = require('express')();
@@ -10,6 +37,15 @@ GameState = function(){
     this.players = [];
     this.bullets = [];
     this.uids = [];
+
+    this.serverTime= 0;
+    this.lastState= {};
+    this.dt = 0;
+    this.lastFrameTime = 0;
+    this.localTime = 0.016;
+
+    this.CreateTimer();
+
 }
 
 GameState.prototype.GetRandomCharacter= function(){
@@ -23,22 +59,88 @@ GameState.prototype.GenerateUid = function(){
     else return newUID;
 }
 
-GameState.prototype.Update = function(){
+GameState.prototype.Update = function(t){
+    this._dt = new Date().getTime();
+    this._dte = new Date().getTime();
 
+    this.dt = this.lastFrameTime ? ((t - this.lastFrameTime)/1000.0):0.16;
+
+    this.lastFrameTime = t;
+
+    this.serverTime = this.localTime;
+
+    this.lastState = {
+        players: this.players,
+        t: this.server_time,
+    }
+
+    for(var i = 0; i<this.players.length; i++){
+        this.players[i].socket.emit('onserverupdate', this.lastState);
+    }
+    
+    this.updateid = window.requestAnimationFrame(this.Update.bind(this), this.viewport);
+}
+GameState.prototype.CreateTimer = function(){
+    setInterval(function(){
+        this._dt = new Date().getTime() - this._dte;
+        this._dte = new Date().getTime();
+        this.localTime += this._dt/1000.0;
+    }.bind(this), 4);
+}
+GameState.prototype.UpdatePhysics= function(){
+    for(var i = 0; i<this.players.length; i++){
+        this.players[i].oldState.pos = this.Pos(this.players[i].pos);
+        var newDir = this.ProcessInput(this.players[i]);
+        this.players[i].pos = this.vAdd(this.players[i].oldState.pos, newDir);
+
+        //check collisions
+
+        this.players[i].inputs = [];
+    }
+}
+GameState.prototype.Pos = function(a){return {x:a.x, y:a.y}; };
+GameState.prototype.vAdd = function(a, b){return {x: a.x+b.x, y:a.y + b.y};};
+
+GameState.prototype.ProcessInput = function(player){
+    var rotationDir = 0;
+    var ic = player.inputs.length;
+    if(ic){
+        for(var i = 0; i<ic; i++){
+            if(player.inputs[i].seq <= player.lastInputSeq) continue;
+
+            var input = player.inputs[i].inputs;
+
+            var c = input.length;
+            for(var j = 0; j<c; j++){
+                var key = input[i];
+                if(key=='l'){
+                    rotationDir-=1;
+                }
+                else if(key=='r'){
+                    rotationDir+=1;
+                }
+            }
+        }
+    }
+
+    //var resulting_vector = 
+    if(player.inputs.length){
+        player.lastInputTime = player.inputs[ic-1].time;
+        player.lastInputSeq = player.inputs[ic-1].seq;
+    }
+    //return resulting_vector;
 }
 
-GameState.prototype.ProcessInput = function(){
-    var
-}
-
-GamePlayer = function(game, uid){
+GamePlayer = function(game, uid, socket){
+    this.socket = socket
     this.game = game;
     this.uid = uid;
 
     this.pos = {x:0, y:0};
-    this.old_state = {pos:{x:0, y:0}};
-    this.cur_state = {pos:{x:0, y:0}};
-    this.state_time = new Date().getTime();
+    this.ship = {};
+    this.oldState = {pos:{x:0, y:0}};
+    this.curState = {pos:{x:0, y:0}};
+    this.stateTime = new Date().getTime();
 
     this.inputs = [];
 }
@@ -53,11 +155,12 @@ app.get('/controller', function(req, res){
 
 
 var game = new GameState();
-
+game.Update(new Date().getTime());
 io.on('connection', function(socket){
     console.log('a user connected')
     //var uid = game.GenerateUid();
-    console.log('assigning uid: '+ uid);
+    //console.log('assigning uid: '+ uid);
+
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
@@ -69,14 +172,18 @@ io.on('connection', function(socket){
         var uid = game.GenerateUid();
         game.uids.push(uid);
 
+        var player = new GamePlayer(game, uid, socket);
+        //game.Update(new Date().getTime());
+
+        socket.emit("connected", uid);
         //create player with uid
     });
-    socket.on('receiveInput', function(controllerCode, input){
+    socket.on('receiveInput', function(controllerCode, input, inputTime, inputSeq){
         var player = game.players.find(function(p){
             return p.uid == controllerCode;
         });
 
-        player.inputs.push(input);
+        player.inputs.push({inputs:input, time:inputTime, seq:inputSeq});
     });
 
     /**
