9a9d4888f37cb3a5e644dba98e87e6c8d4f9a0fe
rylankasitz <rylankasitz@ksu.edu>
2017-11-04 16:39:27 -0600

    testing rotation

--- www/JS/Controller.js
+++ www/JS/Controller.js
@@ -1,41 +1,35 @@
-window.addEventListener('touchenter', function(event) {
-    //var x = event.pageX - window.offsetLeft;
-    alert(e.changedTouches[0].pageX)
-    //document.write("asdfa");
-    //document.getElementById('text').innerHTML = "text";
-
-    /*if(x < window.innerWidth/2) {
-        console.log("engine");
-    }
-    else {
-        console.log("fire");
-    }*/
-    e.preventDefault()
-}, false);
-
-window.addEventListener('keydown', function(event) {
-
-    if(event.keyCode == 65) {
-        console.log("rotate left");
-    }
-    else if(event.keyCode == 68) {
-        console.log("rotate right");
-    }
-    
-}, false);
-
-
 window.ondevicemotion = function(event) {
     ax = event.accelerationIncludingGravity.x
     ay = event.accelerationIncludingGravity.y
     az = event.accelerationIncludingGravity.z
     rotation = event.rotationRate;
-    //document.write("asd");
     if (rotation != null) {
       arAlpha = Math.round(rotation.alpha);
       arBeta = Math.round(rotation.beta);
       arGamma = Math.round(rotation.gamma);
-      //document.write(arAlpha);
+      document.write(rotation.alpha + ", " + rotation.arBeta + ", " + rotation.gamma);
+    }   
+}
+
+// Find the right method, call on correct element
+function launchIntoFullscreen(element) {
+    if(element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if(element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if(element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen();
+    } else if(element.msRequestFullscreen) {
+      element.msRequestFullscreen();
     }
-    
-  }
+}
+
+function Go() {
+    alert("go");
+    launchIntoFullscreen(document.documentElement);
+}
+
+function Fire() {
+    alert("fire")
+    launchIntoFullscreen(document.documentElement);
+}
