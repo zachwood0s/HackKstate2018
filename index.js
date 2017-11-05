0e63e10a9c8cf481aac41cdb66394a092f540391
zachwood0s <zach@hayzak.com>
2017-11-04 21:11:05 -0500

    Merge branch 'master' of https://github.com/zachwood0s/HackKstate2018
    fixed loading of css

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ if('undefined' != typeof(global)) frame_time = 16; //on server we run at 45ms, 2
 
 }() );
 
-
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
 
@@ -155,7 +155,7 @@ GamePlayer = function(uid){
     this.inputs = [];
 }
 
-
+app.use(express.static(__dirname+'/www'))
 app.get('/', function(req, res){
     res.sendFile(__dirname+'/www/index.html');
 });
